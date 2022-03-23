e15fa684b47d2922620641008137d76d9e6059dc
LudMono <theblamno@gmail.com>
2022-03-23 11:09:48 +0300

    release: final version

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const navigation = document.querySelector('#navigation');
-const header = document.querySelector('#header');
-const drawer = document.querySelector('#drawer');
-const toggler = document.querySelector('#toggler');
-const aside = document.querySelector('#aside');
+const navigation = document.querySelector("#navigation");
+const header = document.querySelector("#header");
+const drawer = document.querySelector("#drawer");
+const aside = document.querySelector("#aside");
 
-if (window.matchMedia("(max-width: 960px)").matches) {
-  header.appendChild(navigation)
-  drawer.appendChild(aside)
-} else {
+const toggler = document.querySelector("#toggler");
+
+const asideNest = document.querySelector("#asideNest");
+const navigationNest = document.querySelector("#navigationNest");
+
+
+function moveBlocks() {
+  if (window.matchMedia("(max-width: 960px)").matches) {
+    header.appendChild(navigation);
+  }
+
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    drawer.appendChild(aside);
+  }
+
+  if (window.matchMedia("(min-width: 960px)").matches) {
+    navigationNest.appendChild(navigation);
+  }
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    asideNest.appendChild(aside);
+
+  }
 }
 
+toggler.addEventListener("click", () => {
+  drawer.classList.toggle("header__drawer--active");
+  toggler.classList.toggle("header__toggler--active");
+});
 
-toggler.addEventListener('click', ()=> {
-  drawer.classList.toggle('header__drawer--active');
-})
+window.addEventListener("resize", moveBlocks);
+moveBlocks();
